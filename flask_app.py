bb96d343e2889d16d4e11eb56c1cc2af3eb48733
Angela V-C <angela.the.vc@gmail.com>
2017-06-22 16:12:30 -0400

    switch to new LR model

--- flask_app.py
+++ flask_app.py
@@ -60,17 +60,17 @@ def get_user_data():
     # this is what the user entered
     screen_name = request.args.get('screen_name')
     try:
-        # get last 100 user tweets
-        twts = api.user_timeline(screen_name = screen_name, count = 100, include_rts = True, tweet_mode = "extended")
+        # get last 200 user tweets
+        twts = api.user_timeline(screen_name = screen_name, count = 200, include_rts = True, tweet_mode = "extended")  
 
-
-
- # this is for taking only the tweets with NO mentions
-        tweetsText = ''
-        tweets = []
-        for tweet in twts:
+    
+        
+        # this is for taking all tweets, with and without mentions
+        tweetsMentions = []
+        tweetsNM = []
+        for tweet in twts: 
             # pull date off before getting _json
-            created_at = tweet.created_at.strftime('%m/%d/%Y')
+            created_at = tweet.created_at.strftime('%m/%d/%Y') 
             tweet = tweet._json #First convert tweets from string to json object.
 
             # collect my mentions & process
@@ -78,23 +78,41 @@ def get_user_data():
             mentions = entities['user_mentions']
             mentions = re.sub(r'[^\x00-\x7f]',r'', str(mentions))
 
-            #only keep if empty
-            if mentions == "[]":
-                text = tweet['full_text']
-                tweets.append([created_at, text])
-                tweetsText += ' ' + text + ' '
+            text = tweet['full_text']
+            # move to different places if empty or not
+            if mentions != "[]":
+                tweetsMentions.append([created_at, text])
+            else:
+                tweetsNM.append([created_at,text])
 
-        vectorizer, forest = joblib.load(basepath + '/trolltrackrapp/model/forest_model.pkl')
-        text = vectorizer.transform([tweetsText])
+            NMtweetsText = ''
+            for tweet in tweetsNM:
+                NMtweetsText += ' ' + tweet[1] + ' '
+
+        vectorizer, forest = joblib.load(basepath + '/trolltrackrapp/model/LR_model.pkl)
+        text = vectorizer.transform([NMtweetsText])
 
         predicted = forest.predict(text)
         probs = forest.predict_proba(text)
 
-        if predicted[0]==1:
-            the_result = "{x} IS at risk for engaging in trolling. Trolling probability = {y}".format(x=screen_name,y=round(probs[0][1],2))
-        else:
-            the_result = "{x} is NOT at risk for engaging in trolling. Trolling probability = {y}".format(x=screen_name,y=round(probs[0][1],2))
+        # look for you forms
+        youForms = ['you','your','yours','yourself','yourselves']
+        mentionTweets = pd.DataFrame(tweetsMentions,columns=['date','tweet'])
+        mentionTweets['contains_you']=mentionTweets['tweet'].str.contains('|'.join(youForms))
 
+        # with negative sentiment
+        mentionTweets['sentiment']=mentionTweets['tweet'].apply(sentiment)
+        mentionTweets = pd.concat([mentionTweets.drop(['sentiment'], axis=1), mentionTweets['sentiment'].apply(pd.Series)], axis=1)
+
+        # together they give possible trolling
+        possibleTrolling = mentionTweets[(mentionTweets['contains_you']==1)&(mentionTweets['compound']<0)]
+
+        if predicted[0]==1: 
+            the_result = "{x} IS at risk for engaging in trolling. Any recent trolling activity is listed below.".format(x=screen_name)
+            recent_tweets = possibleTrolling
+        else:
+            the_result = "{x} is NOT at risk for engaging in trolling. Result based on recent tweets below.".format(x=screen_name)
+            recent_tweets = tweetsNM 
 
     except tweepy.TweepError as e:
         the_result = "Sorry, not a valid user name."
